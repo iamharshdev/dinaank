f8972db2759b668163d044544fdac28268833f68
iamharshdev <nitingoswami473@gmail.com>
2021-08-15 13:21:25 +0530

    Add create elem func

--- src/helpers/notJquery.js
+++ src/helpers/notJquery.js
@@ -1,24 +1,25 @@
-const $ = (element) => {
-  return document.querySelectorAll(element);
-};
-const $attr = (elem, attr) => {
-  return elem.getAttribute(attr);
-};
+const $ = (element) => document.querySelectorAll(element);
+
+const $attr = (elem, attr) => elem.getAttribute(attr);
+
 const $class = (elem, className) => {
-  if (elem.classList.contains(className)) {
-    elem.classList.remove(className);
-  } else {
-    elem.classList.add(className);
-  }
+  elem.classList.contains(className)
+    ? elem.classList.remove(className)
+    : elem.classList.add(className);
 };
 
-const $hasClass = (elem, className) => {
-  return elem.classList.contains(className);
+const $hasClass = (elem, className) => elem.classList.contains(className);
+
+const $create = (type, classes) => {
+  const elem = document.createElement(type);
+  elem.setAttribute("class", classes);
+  return elem;
 };
 
-module.exports = {
+export {
   $,
   $attr,
   $class,
   $hasClass,
+  $create,
 };
